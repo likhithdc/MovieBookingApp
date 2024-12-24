abc94fc1e03a71e66905ca9f8ecc71c58c10cfa8
Likhith <likhithdc@gmail.com>
2024-12-24 13:19:00 +0530

    Added code to createBooking from when user clicks on the bookticket and booking is createdin table

--- src/main/java/com/moviebooking/app/service/BookingService.java
+++ src/main/java/com/moviebooking/app/service/BookingService.java
@@ -1,6 +1,7 @@
 package com.moviebooking.app.service;
 
 import com.moviebooking.app.model.Booking;
+import com.moviebooking.app.model.Movie;
 import com.moviebooking.app.repository.BookingRepository;
 import com.moviebooking.app.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,19 +14,29 @@ import java.util.List;
 public class BookingService {
 
     @Autowired
-    private BookingRepository bookingRepository;
-    @Autowired
-    private MovieRepository movieRepository;
+    private final BookingRepository bookingRepository;
 
+    @Autowired
+    private final MovieRepository movieRepository;
+
+    public BookingService(BookingRepository bookingRepository, MovieRepository movieRepository) {
+        this.bookingRepository = bookingRepository;
+        this.movieRepository = movieRepository;
+    }
 
     public List<Booking> getBookingsByUser(Long userId) {
         return bookingRepository.findByUserId(userId);
     }
 
-    public Booking createBooking(Booking booking)
-    {
+    public Booking createBooking(Booking booking) {
         return bookingRepository.save(booking);
     }
 
+    public void createBooking(Long movieId) {
+        Movie movie = movieRepository.findById(movieId).orElseThrow(() -> new RuntimeException(("Movie not found")));
+        Booking booking = new Booking();
+        booking.setMovie(movie);
+        bookingRepository.save(booking);
+    }
 
 }
